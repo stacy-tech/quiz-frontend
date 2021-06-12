d09aa56e06d6cf96ade07ac3a0427316828f78f3
stacy <stacydn23@gmail.com>
2021-06-12 16:18:39 -0400

    update my fetch request

--- src/adapters/quizzesApi.js
+++ src/adapters/quizzesApi.js
@@ -3,8 +3,11 @@ class QuizzesApi {
         this.baseUrl = "http://localhost:3000/quizzes"
     }
 
-    async getQuizzes() {
-        const res = await fetch(this.baseUrl)
-        return res.json
+    getQuizzes() {
+        return fetch(this.baseUrl).then(res => res.json())
     }
+    // async getQuizzes() {
+    //     const res = await fetch(this.baseUrl)
+    //     return res.json
+    // }
 }
