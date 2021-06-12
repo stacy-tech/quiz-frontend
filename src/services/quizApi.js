3ba1e98703d4cac9fb37320467083450ec1e186a
stacy <stacydn23@gmail.com>
2021-06-12 15:42:04 -0400

    structure my javascript/sources folder as well as my index.js file which will kick off my app by creating a new App object

--- src/services/quizApi.js
+++
@@ -1,9 +0,0 @@
-class QuizApi {
-    constructor() {
-        this.baseUrl = 'http://localhost:3000/quizzes'
-    }
-
-    getQuiz() {
-        return fetch(this.baseUrl).then(res => res.json())
-    }
-}
