05ebda80b6f9e80f6408998f223b9fc6947379ee
stacy <stacydn23@gmail.com>
2021-06-12 16:02:14 -0400

    structure my quizzes model which will fetch and load my quizapi

---
+++ src/components/quizzes.js
@@ -0,0 +1,13 @@
+class Quizzes {
+    constructor() {
+        this.quizzes = []
+        this.adapter = new QuizzesApi()
+        this.fetchAndLoadQuizzes()
+    }
+
+    fetchAndLoadQuizzes() {
+        this.adapter.getQuizzes().then(quizzes => {
+            console.log(quizzes)
+        })
+    }
+}
