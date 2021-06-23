3f59d68fffe46dc33e90233b852e17d7b995f18b
stacy <stacydn23@gmail.com>
2021-06-22 20:34:46 -0400

    add event listeners to my answer buttons as well as style my web page

--- src/components/quiz.js
+++ src/components/quiz.js
@@ -1,5 +1,6 @@
 class Quiz {
     static quizzes = []
+    static score = 0
     constructor({id, name, description}) {
         this.name = name
         this.id = id
@@ -22,16 +23,42 @@ class Quiz {
         document.querySelector(".container").appendChild(div)
     
     }
-    
-    findQuestionsByQuizId(){
-       return Question.questions.filter(question => question.quiz_id === this.id)
+
+    answerQuestion(event) {
+        console.log('answering question', event)
+        let chosenAnswer = event.target.innerText
+        console.log(chosenAnswer)
+        let questionId = parseInt(event.target.classList[2])
+        console.log(questionId)
+        var currentQuestionList = Question.questions.filter(question => question.id === questionId)
+        if(currentQuestionList[0].answer === chosenAnswer) {
+            alert('Correct')
+            event.target.style.background = 'yellow'
+            Quiz.score += 1
+        }
+        else {
+            alert('Incorrect')
+            event.target.style.background = 'red'
+            Quiz.score -= 1
+        }
+        debugger
+        Array.from(event.target.parentElement.children).forEach(element => {
+            element.removeEventListener('click', () => console.log("click"))
+            
+        });
+        
+
+        console.log('result', this.result)
     }
-    renderQuestions(event){
+
+    
+    renderQuestions(){
     
         document.querySelector(".container").innerHTML = `
          <h1 id ="quiz-${this.id}">Test your Knowledge of ${this.name}</h1>
              <div id="questions">
                 <h3>Questions</h3>
+                <button id="score-btn" class="score-btn btn">Score</button>
                 <ul></ul>
                 
 
@@ -39,19 +66,29 @@ class Quiz {
             
     
         `
-        this.findQuestionsByQuizId().forEach(question => { 
+        var filteredQuestions = Question.questions.filter(question => question.quiz_id === this.id)
+        
+        
+        
+        filteredQuestions.forEach(question => { 
             document.querySelector("#questions ul").innerHTML +=
             `
                 <li id ="question-${question.id}">
                     <h4> ${question.question}</h4>
                     <div id="answer-buttons" class="btn-grid">
-                        <button class="btn">${question.option1}</button>
-                        <button class="btn">${question.option2}</button>
-                        <button class="btn">${question.answer}</button>
+                        <button class="btn answerBtn ${question.id}">${question.option1}</button>
+                        <button class="btn answerBtn ${question.id}">${question.option2}</button>
+                        <button class="btn answerBtn ${question.id}">${question.answer}</button>
                     </div>
 
                 </li>
             `
         }) 
+
+        var answerButtonClass = document.getElementsByClassName("answerBtn")
+        for (var i = 0; i < answerButtonClass.length; i++) {
+            answerButtonClass[i].addEventListener('click', this.answerQuestion.bind(this))
+        }
+
     }
 }
