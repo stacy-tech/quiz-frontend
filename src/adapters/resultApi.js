3dacb6d625e65ffff772c7d5e7d77626460bdf0b
stacy <stacydn23@gmail.com>
2021-06-30 01:20:07 -0400

    structure my fetch post request

--- src/adapters/resultApi.js
+++ src/adapters/resultApi.js
@@ -1,5 +1,24 @@
 class ResultApi{
+    debugger
     static postResult(target) {
+        fetch('http://localhost/3000/result', {
+            method: 'POST',
+            headers: {
+                "Content_Type": 'application/json',
+                "Accept": 'application/json'
+            },
+            body: JSON.stringify({
+                name: "",
+                score: ""
+            })
+            
+        })
+        .then(res => res.json())
+        .then (json => {
+            let result = new Result(json)
+            new Result(result)
+
+        })
        
     }
 }
